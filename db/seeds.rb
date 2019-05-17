b764838c0ae46af3a6cad46da364a4d1ef0e2ff1
Andrew Allen <asallen67@gmail.com>
2019-05-17 12:52:21 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.destroy_all
+#...
+
+Student.create(first_name: 'Andrew', last_name: 'Allen')
+Student.create(first_name: 'Han', last_name: 'Xu')
